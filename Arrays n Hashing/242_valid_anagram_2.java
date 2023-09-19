319a0d22689c414f7895b2adadfe0668ec70f917
Yao Wang <dell9459@gmail.com>
2023-09-19 08:25:43 -0400

    update

--- Arrays n Hashing/242_valid_anagram_2.java
+++ Arrays n Hashing/242_valid_anagram_2.java
@@ -1,3 +1,23 @@
-public class 242_valid_anagram_2 {
-    
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] charArray1 = s.toCharArray();
+        char[] charArray2 = t.toCharArray();
+        int[] countArray = new int[256];
+
+        for(int i = 0; i< charArray1.length; i ++) {
+            countArray[charArray1[i]]++;
+        }
+        for(int i = 0; i< charArray2.length; i ++) {
+            countArray[charArray2[i]]--;
+        }
+
+        for(int i =0; i < countArray.length; i++) {
+            if(countArray[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 }
